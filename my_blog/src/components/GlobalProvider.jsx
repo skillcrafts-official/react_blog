c312a3d5f00a62051eb39678c2c6fac6dddbb504
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-27 10:48:25 +0300

    The global context API has been successfully updated to version 2025.11.0

--- my_blog/src/components/GlobalProvider.jsx
+++ my_blog/src/components/GlobalProvider.jsx
@@ -1,13 +1,25 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 const GlobalContext = createContext();
 
 function GlobalProvider({ children }) {
-    const isLogin = localStorage.getItem("auth:access_token") ? "login" : "logout"
+    const isLogin = localStorage.getItem("auth:accessToken") ? "login" : "logout"
     const [userId, setUserId] = useState("");
     const [accessToken, setAccessToken] = useState("");
     const [logStatus, setLogStatus] = useState(isLogin);
     const [updatedProfile, setUpdatedProfile] = useState(0);
+    const [profileStatus, setProfileStatus] = useState('old');
+
+    useEffect(() => {
+      const storedUser = localStorage.getItem('auth:userId');
+      const storedToken = localStorage.getItem('auth:accessToken');
+        
+      if (storedUser && storedToken) {
+        setUserId(storedUser);
+        setAccessToken(storedToken)
+        setLogStatus('login');
+      }
+    }, []);
 
     function handleUserId(id) {
         setUserId(id);
@@ -22,8 +34,9 @@ function GlobalProvider({ children }) {
     return (
         <GlobalContext.Provider
             value={{
-                accessToken, logStatus, userId, updatedProfile,
-                handleAccess, handleLogStatus, handleUserId, setUpdatedProfile
+                accessToken, logStatus, userId, updatedProfile, profileStatus,
+                handleAccess, handleLogStatus, handleUserId, setUpdatedProfile,
+                setProfileStatus
             }}>
             {children}
         </GlobalContext.Provider>
