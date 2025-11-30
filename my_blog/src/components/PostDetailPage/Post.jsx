de9c719f1f77a030855630cd1f25174f5b43e617
mrmcmva <vsevolodmartynyuk@yandex.ru>
2025-11-30 20:55:30 +0300

    The PostDetail.jsx has been successfully redirected to new directory

--- my_blog/src/components/PostDetailPage/Post.jsx
+++ my_blog/src/components/PostDetailPage/Post.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import Comments from "../generals/Comments"
+import Comments from "../singles/Comments"
 
 function Post() {
     return (
--- my_blog/src/pages/Post/PostDetail.jsx
+++ my_blog/src/pages/Post/PostDetail.jsx
@@ -1,4 +1,4 @@
-import Post from "../components/PostDetailPage/Post"
+// import Post from "../components/PostDetailPage/Post"
 
 function PostDetail() {
     return (
